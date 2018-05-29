de39b3860cc696d24675d3ffb07aa7212c0c5cb0
fortCollinDev <38673343+fortCollinDev@users.noreply.github.com>
2018-05-29 15:29:09 -0600

    new script to run in vanilla R

---
+++ testdocs/part5/libraryInstalls.r
@@ -0,0 +1,8 @@
+# List of necessary packages for tutorial 
+
+install.packages('randomForest')
+install.packages('rfUtilities')
+install.packages('raster')
+install.packages('rgdal')
+install.packages('corrplot')
+
